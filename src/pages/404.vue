00d5de744e1c0ccb3ee9b7f33be741631c1f60f2
Natalya <NatalyaArt2019@yandex.ru>
2022-03-31 16:34:51 +0300

    My first commit

---
+++ src/pages/404.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="not-found">
+        <h1>
+            Ничего не найдено
+        </h1>
+        <router-link class="back"
+                        :to="{ name: 'main' }"
+                >
+                   Назад к фильмам
+                </router-link>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .not-found {
+         display: flex;
+         flex-direction: column;
+         justify-content: center;
+         align-items: center;
+         min-height: 100vh;
+         h1 {
+             color: #ffffff;
+             font-size: 70px;
+             margin-bottom: 20px;
+         }
+         
+     }
+     .back {
+            color: #ffffff;
+    
+            margin-top: 30px;
+            padding: 2px 4px;
+            background-color: #EB5804;
+            text-decoration: none;
+            border-radius: 5px;
+            justify-content: center;
+            display: flex;
+            width: max-content;
+     }
+</style>
